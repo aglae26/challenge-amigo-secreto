40084c19e9e4b6efbf609b60a5f8dc74299586b2
Aglae Duin <aglaefduin@gmail.com>
2025-08-27 09:56:50 -0500

    Crea un elemento para cada nombre

--- app.js
+++ app.js
@@ -24,4 +24,8 @@ input.value = ''
 function actualizarLista() {
   const lista = document.getElementById('listaAmigos')
   lista.innerHTML = ''
+
+  for (let i = 0; i < amigos.length; i++) {
+    lista.innerHTML += `<li>${amigos[i]}</li>`
+  }
 }
