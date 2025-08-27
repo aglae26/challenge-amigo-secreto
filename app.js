59aacb3f4eb3e67f3aa123b7460e841465305430
Aglae Duin <aglaefduin@gmail.com>
2025-08-27 09:31:53 -0500

    Verifica si fue ingresado un nombre

--- app.js
+++ app.js
@@ -6,4 +6,10 @@ let amigos = []
 // Función para agregar un amigo a la lista.
 function agregarAmigo() {
   const input = document.getElementById('nombreAmigo')
+
+  // Verifica que el input no esté vacío.
+  if (nombreAmigo === '') {
+    alert('Por favor, ingresa un nombre.')
+    return
+  }
 }
